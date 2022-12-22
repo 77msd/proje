cfb3c1a0135a723645517bd42baab7315c8706a2
77msd <115848669+77msd@users.noreply.github.com>
2022-12-22 15:55:41 +0300

    Add files via upload

--- simulasyon.py
+++ simulasyon.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 
 def make_window(width: int, height: int, caption: str) -> pygame.Surface:
-    """Create and return a pygame window."""
+    """Bir pygame penceresi oluşturun ve döndürün."""
     screen: pygame.Surface
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption(caption)
