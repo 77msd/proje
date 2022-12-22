eaf7a0abb8c74556265aa9502e7114bbcfed96a6
Ysf16 <117942587+Ysf16@users.noreply.github.com>
2022-12-22 15:29:38 +0300

    Add files via upload

--- simulasyon.py
+++ simulasyon.py
@@ -1,10 +1,19 @@
-import math
-import pygame
-pygame.init()
-
-def make_window(width: int, height: int, caption: str) -> pygame.Surface:
-    """Create and return a pygame window."""
+def main() -> None:
+    """GÖRÜNTÜYÜ HAREKET ETTİRMEK İÇİN."""
+    # DEĞİŞKENLERE AÇIKLAMA EKLEME
+    SCREEN_HEIGHT: int = 480
+    SCREEN_WIDTH: int = 640
     screen: pygame.Surface
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption(caption)
-    return screen
+    background: pygame.Surface
+    user_quit: bool = False
+    e: pygame.event.Event
+    projectile: pygame.Surface
+    flag: pygame.Surface
+    start_x: float = 0
+    start_y: float = -100
+    x: float = start_x
+    y: float = start_y
+    time: float = 0
+    shoot: bool = False
+    angle: float = 0 
+    speed: float = 100 
