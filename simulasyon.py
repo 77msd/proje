eed6d25f3b86e27fd908fd50292b86a40d968ece
77msd <115848669+77msd@users.noreply.github.com>
2022-12-22 15:16:06 +0300

    Add files via upload

---
+++ simulasyon.py
@@ -0,0 +1,10 @@
+import math
+import pygame
+pygame.init()
+
+def make_window(width: int, height: int, caption: str) -> pygame.Surface:
+    """Create and return a pygame window."""
+    screen: pygame.Surface
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption(caption)
+    return screen
